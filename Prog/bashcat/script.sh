624176455c58032dacc0a85f7eeae314ceee684a
WhiteRose <mathieu.bannwarth@proton.me>
2023-10-22 18:44:42 +0200

    bashcat amélioration

--- Prog/bashcat/script.sh
+++ Prog/bashcat/script.sh
@@ -1,17 +1,17 @@
-exec 3<>/dev/tcp/bashcat/666
-a="\""
-while read -r response; do
-        echo $response
-        n=($(echo $response | tr " " "\n"));
-        if [ "${n[0]}" = "Quelle" ]
-        then
+        exec 3<>/dev/tcp/bashcat/666
+        a="\""
+
+        function find(){
+                n=$1
+                # Permet de savoir si il faut renvoyer la taille ou non.
+                v=$2
+
                 i=${n[7]}
                 champ=${n[16]}
                 champ=${champ::-2}
                 champ=${champ:1}
                 champ="$a$champ$a"
                 cat list.csv | while read line; do
-
                         n=($(echo $line | tr "," "\n"));
                         if [ "${n[0]}" = $champ ]
                         then
@@ -19,28 +19,24 @@ while read -r response; do
                                 t=${t:1}
                                 t=${t::-1}
                                 t="$t"
-                                echo $t >&3
+                                if [ $v = 1 ]
+                                then
+                                        echo $t >&3
+                                else
+                                        echo ${#t} >&3
+                                fi
                         fi
-                done
-        elif [ "${n[0]}" = "Combien" ]
-        then
-                echo ${n[@]}
-                i=${n[7]}
-                champ=${n[16]}
-                champ=${champ::-2}
-                champ=${champ:1}
-                champ="$a$champ$a"
-                cat list.csv | while read line; do
+                done   
+        }
 
-                        n=($(echo $line | tr "," "\n"));
-                        if [ "${n[0]}" = $champ ]
-                        then
-                                t="${n[i-1]}"
-                                t=${t:1}
-                                t=${t::-1}
-                                t="$t"
-                                echo ${#t} >&3
-                        fi
-                done
-        fi
-done <&3
+        while read -r response; do
+                echo $response
+                n=($(echo $response | tr " " "\n"));
+                if [ "${n[0]}" = "Quelle" ]
+                then
+                        find $n 1
+                elif [ "${n[0]}" = "Combien" ]
+                then
+                        find $n 0
+                fi
+        done <&3
